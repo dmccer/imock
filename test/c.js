91ae116269222a71ab7503ef17f9c43e200b1b00
yunhua.xiao <yunhua.xiao@dianping.com>
2014-09-01 17:59:24 +0800

    v2

---
+++ test/c.js
@@ -0,0 +1,30 @@
+module.exports = {
+    get: function (req, res) {
+        res.json(200, {
+            code: 200,
+            msg: {
+                shopName: '小南国',
+                shopId: '1234',
+                level: 5
+            }
+        });
+    },
+
+    put: function (req, res) {
+        res.json(200, {
+            code: 200,
+            msg: '修改成功'
+        });
+    },
+
+    post: function (req, res) {
+        res.json(200, {
+            code: 200,
+            msg: '保存成功'
+        });
+    },
+
+    update: function () {},
+
+    patch: function () {}
+};
